83636611dd47fcccb910ee7e885e417dd2076a05
list <49289744+hyunhan3008@users.noreply.github.com>
2022-04-10 10:32:59 +0900

    basic flutter format with some widgets

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+// void main () {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  void answerQuestion() {
+    print('Answer has been chosen!');
+  }
+
+  @override
+  var questions = [
+    'What\'s your favourite colour?',
+    'What\'s your favourite animal'
+  ];
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            RaisedButton(child: Text('Answer1'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer1'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer1'), onPressed: answerQuestion),
+          ],
+        ),
+      ),
+    );
+  }
+}
